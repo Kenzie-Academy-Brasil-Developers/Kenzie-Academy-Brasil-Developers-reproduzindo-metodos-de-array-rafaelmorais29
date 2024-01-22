a29b6ebcf58bdc760a007332d3ef564c7bcea0a2
rafaelmorais29 <rafaelmachadomorais29@gmail.com>
2024-01-22 13:48:42 -0300

    Criação dos métodos

--- src/scripts/metodos.js
+++ src/scripts/metodos.js
@@ -4,18 +4,54 @@
   -------------------
 */
 
-function newMap() {
-  // Reproduza aqui o seu método map
+function newMap(array, callback) {
+  const result = [];
+
+  for (let index = 0; index < array.length; index++) {
+    const currentValue = array[index];
+    const mappedValue = callback(currentValue, index, array);
+    result.push(mappedValue);
+  }
+
+  return result;
 }
 
-function newFilter() {
-  // Reproduza aqui o seu método filter
+function newFilter(array, callback) {
+  const result = [];
+
+  for (let index = 0; index < array.length; index++) {
+    const currentValue = array[index];
+
+    if (callback(currentValue, index, array)) {
+      result.push(currentValue);
+    }
+  }
+
+  return result;
 }
 
-function newFind() {
-  // Reproduza aqui o seu método find
+function newFind(array, callback) {
+  for (let index = 0; index < array.length; index++) {
+    const currentValue = array[index];
+
+    if (callback(currentValue, index, array)) {
+      return currentValue;
+    }
+  }
+
+  return undefined;
 }
 
-function newReduce() {
-  // Reproduza aqui o seu método reduce
+function newReduce(array, callback, initialValue) {
+  let accumulator = initialValue !== undefined ? initialValue : array[0];
+  let startIndex = initialValue !== undefined ? 0 : 1;
+
+  for (let index = startIndex; index < array.length; index++) {
+    const currentValue = array[index];
+    accumulator = callback(accumulator, currentValue, index, array);
+  }
+
+  return accumulator;
 }
+
+export { newMap, newFilter, newFind, newReduce };
