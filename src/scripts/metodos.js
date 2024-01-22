62af92fd7a9379213113f31393caebabadd3fd8c
Rafael Machado Morais <132380805+rafaelmorais29@users.noreply.github.com>
2024-01-22 13:38:47 -0300

    Initial commit

---
+++ src/scripts/metodos.js
@@ -0,0 +1,21 @@
+/*
+  -------------------
+  Reprodução dos Métodos
+  -------------------
+*/
+
+function newMap() {
+  // Reproduza aqui o seu método map
+}
+
+function newFilter() {
+  // Reproduza aqui o seu método filter
+}
+
+function newFind() {
+  // Reproduza aqui o seu método find
+}
+
+function newReduce() {
+  // Reproduza aqui o seu método reduce
+}
